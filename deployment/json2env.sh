924cd1059f61b96c4b563f15adc36a773b3ad415
Rocky <49852443+rakeshSgr@users.noreply.github.com>
2026-09-04 13:00:48 +0530

    Update json2env.sh

--- deployment/json2env.sh
+++ deployment/json2env.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jq -r 'to_entries[] | .key as $k | (.value | if type == "string" then . else tojson end) as $v | "\($k)=\($v)"' "$1" > "$2"
+jq -r 'to_entries[] | .key as $k | (.value | if type == "string" then . else tojson end) as $v | "\($k)=\($v)"'
